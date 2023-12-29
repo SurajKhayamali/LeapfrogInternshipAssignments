84e65b76db6f0dcf47993530aa93755b9a4f430d
Suraj Khayaamali <surajkhayamali.sk@gmail.com>
2023-12-29 11:13:28 +0545

    RESTful API: Routes setup with url params

---
+++ Week 6/Day 5/rest-api/src/controllers/user.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+
+// import * as userService from "../services/user";
+
+// export async function createUser(req: Request, res: Response) {
+//   const { name, email, password } = req.body;
+
+//   const user = await userService.createUser(name, email, password);
+
+//   res.status(201).json(user);
+// }
+
+export async function getUsers(req: Request, res: Response) {
+  // const users = await userService.getUsers();
+
+  // res.json(users);
+
+  res.json({
+    message: "Hello World!",
+  });
+}
+
+export async function getUserById(req: Request, res: Response) {
+  const { id } = req.params;
+  // const user = await userService.getUserById(parseInt(id));
+  // res.json(user);
+
+  res.json({
+    message: `User with id ${id} found!`,
+  });
+}
+
+// export async function updateUser(req: Request, res: Response) {
+//   const { id } = req.params;
+//   const { name, email, password } = req.body;
+
+//   const user = await userService.updateUser(
+//     parseInt(id),
+//     name,
+//     email,
+//     password
+//   );
+
+//   res.json(user);
+// }
+
+// export async function deleteUser(req: Request, res: Response) {
+//   const { id } = req.params;
+
+//   await userService.deleteUser(parseInt(id));
+
+//   res.sendStatus(204);
+// }
--- Week 6/Day 5/rest-api/src/index.ts
+++ Week 6/Day 5/rest-api/src/index.ts
@@ -1,13 +1,17 @@
 import express, { Request, Response } from "express";
+
 import config from "./config";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({
-    message: "Hello World!",
-  });
-});
+app.use(routes);
+
+// app.get("/", (req: Request, res: Response) => {
+//   res.json({
+//     message: "Hello World!",
+//   });
+// });
 
 console.log(`Server listening on port: ${config.serverPort}`);
 
