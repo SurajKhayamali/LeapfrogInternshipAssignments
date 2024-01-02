32dd4e7d840a07882cd584091b7b5de5c4f56c9c
Suraj Khayamali <surajkhayamali.sk@gmail.com>
2024-01-03 00:54:32 +0545

    update: adding validation on login endpoint

--- Week 6/Day 5/nodejs-assignment-1/src/routes/auth.route.ts
+++ Week 6/Day 5/nodejs-assignment-1/src/routes/auth.route.ts
@@ -5,14 +5,14 @@ import {
   handleRefreshToken,
   handleLogout,
 } from '../controllers/auth.controller';
-import { signUpSchema } from '../schemas/auth.schema';
+import { loginSchema, signUpSchema } from '../schemas/auth.schema';
 import { validateReqBody } from '../middlewares/validator.middleware';
 
 const router = Router();
 
 router.post('/signup', validateReqBody(signUpSchema), handleSignup);
 
-router.post('/login', handleLogin);
+router.post('/login', validateReqBody(loginSchema), handleLogin);
 
 router.post('/refresh', handleRefreshToken);
 
--- Week 6/Day 5/nodejs-assignment-1/src/schemas/auth.schema.ts
+++ Week 6/Day 5/nodejs-assignment-1/src/schemas/auth.schema.ts
@@ -1,12 +1,18 @@
 import Joi from 'joi';
 
 export const signUpSchema = Joi.object({
-  name: Joi.string().min(3).max(30).regex(/\s/).required().messages({
-    'string.min': 'Name must be at least 3 characters long',
-    'string.max': 'Name must be at most 30 characters long',
-    'string.pattern.base': 'Name must be a full name, with a space in between',
-    'any.required': 'Name is required',
-  }),
+  name: Joi.string()
+    .min(3)
+    .max(30)
+    .pattern(new RegExp('\\s'))
+    .required()
+    .messages({
+      'string.min': 'Name must be at least 3 characters long',
+      'string.max': 'Name must be at most 30 characters long',
+      'string.pattern.base':
+        'Name must be a full name, with a space in between',
+      'any.required': 'Name is required',
+    }),
   email: Joi.string().email().messages({
     'string.email': 'Email must be a valid email',
   }),
@@ -22,6 +28,25 @@ export const signUpSchema = Joi.object({
     }),
   password: Joi.string().min(8).required().messages({
     'string.min': 'Password must be at least 8 characters long',
+    'any.required': 'Password is required',
+  }),
+});
+
+export const loginSchema = Joi.object({
+  emailOrUsername: Joi.alternatives()
+    .try(
+      Joi.string().email(),
+      Joi.string().min(3).max(20).pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
+    )
+    .required()
+    .messages({
+      'alternatives.types':
+        'Email or username must be a valid email or username',
+      'alternatives.match':
+        'Email or username must be either a valid email or a username',
+      'any.required': 'Email or username is required',
+    }),
+  password: Joi.string().required().messages({
     'any.required': 'Password is required',
   }),
 });
